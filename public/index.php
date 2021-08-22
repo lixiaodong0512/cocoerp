f5171fbc618211ed77b174163ddc56d342da37e5
li.xiaodong0512 <li.xiaodong0512@gmail.com>
2021-08-22 21:15:52 +0800

    跨域

--- public/index.php
+++ public/index.php
@@ -5,6 +5,14 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// 制定允许其他域名访问
+header("Access-Control-Allow-Origin:*");
+// 响应类型
+header('Access-Control-Allow-Methods:*');
+// 响应头设置
+header('Access-Control-Allow-Headers:x-requested-with, content-type');
+
+
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
